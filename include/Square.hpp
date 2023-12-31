35bb7571a2d4a0ecb7aa7f408639a1f2358e897e
PlungedInCode <plungedincode@gmail.com>
2023-12-31 18:25:06 +0300

    Square

--- include/Square.hpp
+++ include/Square.hpp
@@ -11,18 +11,8 @@ public:
     Circle circumscribedCircle() const;
     Circle inscribedCircle() const;
 
+    friend std::ostream& operator<<(std::ostream& out, const Rectangle& rect);
 
-    // Implement virtual methods from Shape class
-    double perimeter() const override;
-    double area() const override;
-    bool operator==(const Shape& another) const override;
-    bool isCongruentTo(const Shape& another) const override;
-    bool isSimilarTo(const Shape& another) const override;
-    bool containsPoint(const Point& point) const override;
-    void rotate(const Point& center, double angle) override;
-    void reflex(const Point& center) override;
-    void reflex(const Line& axis) override;
-    void scale(const Point& center, double coefficient) override;
 private:
 
 };
