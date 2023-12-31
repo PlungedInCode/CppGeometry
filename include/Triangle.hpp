7e2042b1bf0417699ba9942602485c0dcf2e859c
PlungedInCode <plungedincode@gmail.com>
2023-12-31 18:25:46 +0300

    Trinagle

--- include/Triangle.hpp
+++ include/Triangle.hpp
@@ -7,7 +7,7 @@ class Triangle : public Polygon {
  public:
   // Constructor
   Triangle(const Point& vertex1, const Point& vertex2, const Point& vertex3);
-
+  ~Triangle();
   // Additional methods specific to Triangle
   Circle circumscribedCircle() const;
   Circle inscribedCircle() const;
@@ -15,16 +15,4 @@ class Triangle : public Polygon {
   Point orthocenter() const;
   Line EulerLine() const;
   Circle ninePointsCircle() const;
-
-  // Override methods from Polygon and Shape
-  double perimeter() const override;
-  double area() const override;
-  bool operator==(const Shape& another) const override;
-  bool isCongruentTo(const Shape& another) const override;
-  bool isSimilarTo(const Shape& another) const override;
-  bool containsPoint(const Point& point) const override;
-  void rotate(const Point& center, double angle) override;
-  void reflex(const Point& center) override;
-  void reflex(const Line& axis) override;
-  void scale(const Point& center, double coefficient) override;
 };
--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -1,171 +1,108 @@
 #include "Triangle.hpp"
+
+#include <iostream>
+
 #include <cmath>
 
 // Constructor
-Triangle::Triangle(const Point& vertex1, const Point& vertex2, const Point& vertex3) : Polygon::Polygon(vertex1, vertex2, vertex3) {
-    // Add vertices in traversal order
-    // addVertex(vertex1);
-    // addVertex(vertex2);
-    // addVertex(vertex3);
-}
+Triangle::Triangle(const Point& vertex1, const Point& vertex2,
+                   const Point& vertex3)
+    : Polygon::Polygon(vertex1, vertex2, vertex3) {}
+
+Triangle::~Triangle(){};
 
 // Additional methods specific to Triangle
 Circle Triangle::circumscribedCircle() const {
-    // Calculate the circumscribed circle of the triangle
-    // Point A = getVertex(0);
-    // Point B = getVertex(1);
-    // Point C = getVertex(2);
+  auto vertices = Polygon::getVertices();
+  Point A = vertices[0];
+  Point B = vertices[1];
+  Point C = vertices[2];
 
-    // double a = B.distanceTo(C);
-    // double b = A.distanceTo(C);
-    // double c = A.distanceTo(B);
+  auto d = 2 * (A.x * (B.y - C.y) + B.x * (C.y - A.y) + C.x * (A.y - B.y));
+  double a = (A.x * A.x + A.y * A.y);
+  double b = (B.x * B.x + B.y * B.y);
+  double c = (C.x * C.x + C.y * C.y);
 
-    // double centerX = (a * A.x + b * B.x + c * C.x) / (a + b + c);
-    // double centerY = (a * A.y + b * B.y + c * C.y) / (a + b + c);
+  double center_x = (a * (B.y - C.y) +  b * (C.y - A.y) + c * (A.y - B.y)) / d;
+  double center_y = (a * (C.x - B.x) +  b * (A.x - C.x) + c * (B.x - A.x)) / d;
 
-    // double radius = A.distanceTo({centerX, centerY});
+  double radius = getDistance(A, {center_x, center_y});
 
-
-    // return Circle({centerX, centerY}, radius);
+  return Circle({center_x, center_y}, radius);
 }
 
 Circle Triangle::inscribedCircle() const {
-    // Calculate the inscribed circle of the triangle
-    // Point A = getVertex(0);
-    // Point B = getVertex(1);
-    // Point C = getVertex(2);
+  auto vertices = Polygon::getVertices();
+  Point A = vertices[0];
+  Point B = vertices[1];
+  Point C = vertices[2];
 
-    // double a = B.distanceTo(C);
-    // double b = A.distanceTo(C);
-    // double c = A.distanceTo(B);
+  double a = getDistance(B, C);
+  double b = getDistance(A, C);
+  double c = getDistance(A, B);
 
-    // double centerX = (a * A.x + b * B.x + c * C.x) / (a + b + c);
-    // double centerY = (a * A.y + b * B.y + c * C.y) / (a + b + c);
+  double centerX = (a * A.x + b * B.x + c * C.x) / (a + b + c);
+  double centerY = (a * A.y + b * B.y + c * C.y) / (a + b + c);
 
-    // double radius = 2 * area() / perimeter();
+  double radius = 2 * area() / perimeter();
 
-    // return Circle({centerX, centerY}, radius);
+  return Circle({centerX, centerY}, radius);
 }
 
 Point Triangle::centroid() const {
-    // Calculate the centroid of the triangle
-    // double centerX = (getVertex(0).x + getVertex(1).x + getVertex(2).x) / 3.0;
-    // double centerY = (getVertex(0).y + getVertex(1).y + getVertex(2).y) / 3.0;
+  auto vertices = Polygon::getVertices();
+  double centerX = (vertices[0].x + vertices[1].x + vertices[2].x) / 3.0;
+  double centerY = (vertices[0].y + vertices[1].y + vertices[2].y) / 3.0;
 
-    // return {centerX, centerY};
+  return {centerX, centerY};
 }
 
+// Point Triangle::orthocenter() const {
+//   auto vertices = Polygon::getVertices();
+//   Point A = vertices[0];
+//   Point B = vertices[1];
+//   Point C = vertices[2];
+
+//   double a = getDistance(B, C);
+//   double b = getDistance(A, C);
+//   double c = getDistance(A, B);
+
+//   double numeratorX = a * a * (A.x - B.x + C.x) + b * b * (B.x - C.x + A.x) +
+//                       c * c * (C.x - A.x + B.x);
+//   double numeratorY = a * a * (A.y - B.y + C.y) + b * b * (B.y - C.y + A.y) +
+//                       c * c * (C.y - A.y + B.y);
+//   double denom = 2 * (a * (B.y - C.y) + b * (C.y - A.y) + c * (A.y - B.y));
+
+//   return {numeratorX / denom, numeratorY / denom};
+// }
 Point Triangle::orthocenter() const {
-    // Calculate the orthocenter of the triangle
-    // Point A = getVertex(0);
-    // Point B = getVertex(1);
-    // Point C = getVertex(2);
-
-    // double a = B.distanceTo(C);
-    // double b = A.distanceTo(C);
-    // double c = A.distanceTo(B);
-
-    // double numeratorX = a * a * (A.x - B.x + C.x) + b * b * (B.x - C.x + A.x) + c * c * (C.x - A.x + B.x);
-    // double numeratorY = a * a * (A.y - B.y + C.y) + b * b * (B.y - C.y + A.y) + c * c * (C.y - A.y + B.y);
-    // double denominator = 2 * (a * (B.y - C.y) + b * (C.y - A.y) + c * (A.y - B.y));
-
-    // return {numeratorX / denominator, numeratorY / denominator};
+  Point circum_center = circumscribedCircle().center();
+  Point centroid_p = this->centroid();
+  double x = 3 * centroid_p.x - 2 * circum_center.x;
+  double y = 3 * centroid_p.y - 2 * circum_center.y;
+  return Point(x, y);
 }
+
+
+// Line Triangle::EulerLine() const {
+//   // std::cout << centroid()
+//   return Line(centroid(), circumscribedCircle().center());
+// }
 
 Line Triangle::EulerLine() const {
-    // Calculate the Euler line of the triangle
-    // return Line(centroid(), circumscribedCircle().center());
+  // Calculate the Euler line of the triangle
+  Point orthocenter = Triangle::orthocenter();
+  Point circumcenter = Triangle::circumscribedCircle().center();
+  std::cout << orthocenter << " " << circumcenter << std::endl;
+  return Line(orthocenter, circumcenter);
 }
 
 Circle Triangle::ninePointsCircle() const {
-    // Calculate the nine points circle of the triangle
-    // Circle circumscribed = circumscribedCircle();
-    // Circle inscribed = inscribedCircle();
+  Circle circumscribed = circumscribedCircle();
+  Circle inscribed = inscribedCircle();
 
-    // Point center = (circumscribed.center() + inscribed.center()) / 2.0;
-    // double radius = (circumscribed.radius() + inscribed.radius()) / 2.0;
+  Point center = (circumscribed.center() + inscribed.center()) / 2.0;
+  double radius = (circumscribed.radius() + inscribed.radius()) / 2.0;
 
-    // return Circle(center, radius);
-}
-
-// Implement virtual methods from Polygon and Shape
-double Triangle::perimeter() const {
-    // Calculate the perimeter of the triangle
-    // return getVertex(0).distanceTo(getVertex(1)) + getVertex(1).distanceTo(getVertex(2)) +
-    //        getVertex(2).distanceTo(getVertex(0));
-}
-
-double Triangle::area() const {
-    // Calculate the area of the triangle
-    // Point A = getVertex(0);
-    // Point B = getVertex(1);
-    // Point C = getVertex(2);
-
-    // return 0.5 * std::abs(A.x * (B.y - C.y) + B.x * (C.y - A.y) + C.x * (A.y - B.y));
-}
-
-bool Triangle::operator==(const Shape& another) const {
-    const Triangle* otherTriangle = dynamic_cast<const Triangle*>(&another);
-
-    if (otherTriangle) {
-        // Compare triangle vertices for equality
-        // return getVertices() == otherTriangle->getVertices();
-    }
-
-    return false;
-}
-
-bool Triangle::isCongruentTo(const Shape& another) const {
-    const Triangle* otherTriangle = dynamic_cast<const Triangle*>(&another);
-
-    if (otherTriangle) {
-        // Two triangles are congruent if they have the same vertices
-        // return getVertices() == otherTriangle->getVertices();
-    }
-
-    return false;
-}
-
-bool Triangle::isSimilarTo(const Shape& another) const {
-    const Triangle* otherTriangle = dynamic_cast<const Triangle*>(&another);
-
-    if (otherTriangle) {
-        // Two triangles are similar if they have the same centroid
-        // return centroid() == otherTriangle->centroid();
-    }
-
-    return false;
-}
-
-bool Triangle::containsPoint(const Point& point) const {
-    // Implement point containment check logic for a triangle
-    return Polygon::containsPoint(point);
-}
-
-void Triangle::rotate(const Point& center, double angle) {
-    // Rotate the vertices of the triangle
-    // for (auto& vertex : getVertices()) {
-    //     vertex.rotate(center, angle);
-    // }
-}
-
-void Triangle::reflex(const Point& center) {
-    // Reflect the vertices of the triangle with respect to the center
-    // for (auto& vertex : getVertices()) {
-    //     vertex.reflex(center);
-    // }
-}
-
-void Triangle::reflex(const Line& axis) {
-    // Reflect the vertices of the triangle with respect to the axis
-    // for (auto& vertex : getVertices()) {
-    //     vertex.reflex(axis);
-    // }
-}
-
-void Triangle::scale(const Point& center, double coefficient) {
-    // Scale the vertices of the triangle and move its center accordingly
-    // for (auto& vertex : getVertices()) {
-    //     vertex.scale(center, coefficient);
-    // }
+  return Circle(center, radius);
 }
