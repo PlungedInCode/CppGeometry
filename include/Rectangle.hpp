b1fbc5f702782aad6c2e41d1914c893ee9cb12d0
PlungedInCode <plungedincode@gmail.com>
2024-01-02 10:56:07 +0300

    Constructor from 2 points, and ratio

--- include/Rectangle.hpp
+++ include/Rectangle.hpp
@@ -6,11 +6,14 @@
 class Rectangle : public Polygon {
 public:
     Rectangle(const Point& point1, const Point& point2);
-    // Rectangle(const double coefficient);
+    Rectangle(const Point& point1, const Point& point2, int ratio);
+    std::vector<Point> chooseVerticesOrder(const Point& point1, const Point& point2, int ratio) const;
     virtual ~Rectangle();
 
     Point center() const;
     std::pair<Line, Line> diagonals() const;
+
+    // bool operator==(const Shape& other) const override;
 
     friend std::ostream& operator<<(std::ostream& out, const Rectangle& rect);
 
--- src/Rectangle.cpp
+++ src/Rectangle.cpp
@@ -4,6 +4,27 @@ Rectangle::Rectangle(const Point& point1, const Point& point2)
     : Polygon::Polygon(point1, Point(point2.x, point1.y), point2,
                        Point(point1.x, point2.y)) {}
 
+Rectangle::Rectangle(const Point& point1, const Point& point2, int ratio)
+    : Polygon(chooseVerticesOrder(point1, point2, ratio)) {}
+
+std::vector<Point> Rectangle::chooseVerticesOrder(const Point& point1,
+                                                  const Point& point2,
+                                                  int ratio) const {
+  double distance1to2 = getDistance(point1, point2);
+  double distanceRatio = distance1to2 / (1.0 + ratio);
+
+  Point p1 = point1;
+  Point p2 = point1 + Point(distanceRatio, 0);
+  Point p3 = point2;
+  Point p4 = point2 - Point(distanceRatio, 0);
+
+  if (point1.x < point2.x) {
+    return {p1, p2, p3, p4};
+  } else {
+    return {p2, p1, p4, p3};
+  }
+}
+
 Rectangle::~Rectangle(){};
 
 Point Rectangle::center() const {
@@ -21,6 +42,17 @@ std::pair<Line, Line> Rectangle::diagonals() const {
 
   return std::make_pair(diag1, diag2);
 }
+
+// bool Rectangle::operator==(const Shape& other) const {
+//     const Rectangle* otherRectangle = dynamic_cast<const Rectangle*>(&other);
+
+//     if (otherRectangle) {
+//         return Polygon::operator==(*otherRectangle); // Compare sets of points
+//     }
+
+//     return false;
+// }
+
 
 std::ostream& operator<<(std::ostream& out, const Rectangle& rect) {
   out << "{";
