0d76ead0ff10d4e89ba429f9d1b5e80f458bec9d
PlungedInCode <plungedincode@gmail.com>
2024-01-05 19:54:03 +0300

    Slope Intercept form

--- include/Line.hpp
+++ include/Line.hpp
@@ -1,24 +1,29 @@
-#pragma once
+#ifndef LINE_HPP
+#define LINE_HPP
 
-#include "Point.hpp"
 #include "Constants.hpp"
+#include "Point.hpp"
 class Line {
-public:
-    Line();
-    Line(const Point& start, const Point& end);
-    Line(const double slope, const double intercept);
-    Line(const Point& point, const double slope);
-    ~Line();
-    
-    bool operator==(const Line& other) const;
-    bool operator!=(const Line& other) const;
+ public:
+  Line();
+  Line(const Point& start, const Point& end);
+  Line(const double slope, const double intercept);
+  Line(const Point& point, const double slope);
+  ~Line();
 
-    friend std::ostream& operator<<(std::ostream& out, const Line& line);
+  double getSlope() const;
+  double getIntercept() const;
 
-    // Ax + By + C = 0
-    double A;
-    double B;
-    double C;
+  Point intersect(const Line& other) const;
+
+  bool operator==(const Line& other) const;
+  bool operator!=(const Line& other) const;
+
+  friend std::ostream& operator<<(std::ostream& out, const Line& line);
+
+ private:
+  double slope_;
+  double intercept_;
 };
 
-Point lineIntersection(const Line& a, const Line& b);
+#endif  // LINE_HPP
--- src/Line.cpp
+++ src/Line.cpp
@@ -1,61 +1,40 @@
 #include "Line.hpp"
 
-// Ax + By + C = 0;
-Line::Line() : A(0), B(0), C(0){};
+using namespace Utils;
 
-//* Two points
-Line::Line(const Point& start, const Point& end) {
-  if (Utils::isEqual(start.x, end.x)) {
-    A = 1;
-    B = 0;
-    C = -start.x;
-  } else if (Utils::isEqual(start.y, end.y)) {
-    A = 0;
-    B = 1;
-    C = -start.y;
-  } else {
-    A = (start.y - end.y) / (start.x - end.x);
-    B = -1;
-    C = (start.x * end.y - end.x * start.y) / (start.x - end.x);
+Line::Line(const Point& start, const Point& end)
+    : slope_((start.y - end.y) / (start.x - end.x)),
+      intercept_(start.y - slope_ * start.x) {
+  if (std::isinf(slope_)) {
+    intercept_ = 0;
   }
 }
 
-//* Slope and shift
 Line::Line(const double slope, const double intercept)
-    : A(slope), B(-1), C(intercept) {}
+    : slope_(slope), intercept_(intercept) {}
 
-//* Point and slope
 Line::Line(const Point& point, const double slope)
-    : A(slope), B(-1), C(point.y - A * point.x) {}
+    : slope_(slope), intercept_(point.y - slope_ * point.x) {}
 
 Line::~Line() {}
 
+double Line::getSlope() const { return slope_; }
+
+double Line::getIntercept() const { return intercept_; }
+
 bool Line::operator==(const Line& other) const {
-  return Utils::isEqual(A, other.A) && Utils::isEqual(B, other.B) &&
-         Utils::isEqual(C, other.C);
+  return isEqual(slope_, other.slope_) && isEqual(intercept_, other.intercept_);
 }
 
 bool Line::operator!=(const Line& other) const { return !(*this == other); }
 
-std::ostream& operator<<(std::ostream& out, const Line& line) {
-  out << line.A << "x +" << line.B << "y + " << line.C;
-  return out;
+Point Line::intersect(const Line& other) const {
+  double new_x = (intercept_ - other.intercept_) / (slope_ - other.slope_);
+  double new_y = slope_ * new_x + intercept_;
+  return {new_x, new_y};
 }
 
-Point lineIntersection(const Line& line1, const Line& line2) {
-  double x, y;
-  if (Utils::isEqual(line1.B, 0.0)) {
-    x = -line1.C / line1.A;
-    y = (-line2.C - line2.A * x) / line2.B;
-  } else if (Utils::isEqual(line1.A, 0.0)) {
-    y = -line1.C / line1.B;
-    x = (-line2.C - line2.B * y) / line2.A;
-  } else {
-    x = (line1.B * line2.C - line2.B * line1.C) /
-        (line2.B * line1.A - line2.A * line1.B);
-    y = (-line1.C - line1.A * x) / line1.B;
-  }
-  if (Utils::isEqual(x, 0.0)) x = fabs(x);
-  if (Utils::isEqual(y, 0.0)) y = fabs(y);
-  return Point(x, y);
+std::ostream& operator<<(std::ostream& out, const Line& line) {
+  out << "y = " << line.getSlope() << "x + " << line.getIntercept();
+  return out;
 }
